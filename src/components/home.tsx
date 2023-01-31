02fea62594576bb22d7813d2fe27f2b878b8b937
thereseml <93775653+thereseml@users.noreply.github.com>
2023-01-31 14:41:14 +0100

    Revert "Develop"

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,3 +1,6 @@
+import { Portfolio } from "./portfolio";
+import { Education } from "./education";
+import { Experience } from "./experience";
 import iconWave from "../img/iconWave.png";
 
 export function Home() {
@@ -5,23 +8,8 @@ export function Home() {
     <>
       <h2>Välkommen!</h2>
       <section className="Home">
-<<<<<<< HEAD
-        <p>
-          Jag heter Therese och är en blivande front end developer, tar examen
-          Juni 2023, som älskar att skapa snygga och användarvänliga
-          webbplatser. Jag har en passion för att lära mig nya saker och är
-          alltid nyfiken på nya tekniker och metoder. Jag har en stor vilja att
-          lära mig och utvecklas som utvecklare. Jag är en driven person som
-          gillar att arbeta i team och har lätt för att samarbeta med andra. Jag
-          är en positiv person som gillar att ha roligt på jobbet och göra andra
-          glada. Jag är lösningsorienterad och löser gärna problem, är bra på
-          att kommunicera och har hög arbetsmoral.
-        </p>
-        <img className="iconWave" src={iconWave} alt="Tjej som vinkar" />
-=======
         <p>Hejhej</p>
         <img src={iconWave} />
->>>>>>> 28ac224bd4892abc59343bd4610442022adc243e
       </section>
     </>
   );
