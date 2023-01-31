b6d1b4812c9e70dc9f79b22894db8b217bbf43b7
thereseml <tm-l@live.se>
2023-01-31 14:30:10 +0100

    animation

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -6,18 +6,20 @@ import iconWave from "../img/iconWave.png";
 export function Home() {
   return (
     <>
+      <h2>Välkommen!</h2>
       <section className="Home">
-        <h2>Home</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
-          lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam
-          viverra nec consectetur ante hendrerit. Donec et mollis dolor.
-          Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam
-          tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut
-          libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit.
+          Jag heter Therese och är en blivande front end developer, tar examen
+          Juni 2023, som älskar att skapa snygga och användarvänliga
+          webbplatser. Jag har en passion för att lära mig nya saker och är
+          alltid nyfiken på nya tekniker och metoder. Jag har en stor vilja att
+          lära mig och utvecklas som utvecklare. Jag är en driven person som
+          gillar att arbeta i team och har lätt för att samarbeta med andra. Jag
+          är en positiv person som gillar att ha roligt på jobbet och göra andra
+          glada. Jag är lösningsorienterad och löser gärna problem, är bra på
+          att kommunicera och har hög arbetsmoral.
         </p>
-        <img src={iconWave} />
+        <img className="iconWave" src={iconWave} />
       </section>
     </>
   );
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,109 +1,61 @@
-import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { RiComputerLine } from "react-icons/ri";
+import qrPet from "../img/qrpet.png";
+import movieShop from "../img/movieshop.png";
+import katanaSushi from "../img/katanasushi.png";
 
 export function Portfolio({}) {
   return (
-    <section className="portfolio">
+    <section className="Portfolio">
       <h2>Portfolio</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
-        lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam
-        viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent
-        et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt
-        congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed
-        arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit.
+        Här är ett litet urval av projekt jag gjort under min utbildning till
+        front end developer. Klicka på ikonerna för att se projekten live eller
+        repot på github.
       </p>
       <div className="flexDiv">
         <div className="flexItem">
-          <h3>Project 1</h3>
+          <h3>Examensarbete (QR Pet)</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
-            diam
+            QR Pet, en fiktiv hemsida där du kan registrera dina husdjur och
+            tillbaka får du en QR kod att sätta i halsbandet. Ifall ditt djur
+            springer bort så kan upphittaren skanna qr koden och få fram dina
+            kontaktuppgifter. Byggt i React.
           </p>
+          <img src={qrPet} />
           <div className="icons">
-            <a href="http://">
+            <a href="https://github.com/thereseml/QRPet">
               <FaGithub />
             </a>
-            <a href="http://">
+            <a href="https://qr-pet.vercel.app/">
               <RiComputerLine />
             </a>
           </div>
         </div>
 
         <div className="flexItem">
-          <h3>Project 1</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
-            diam
-          </p>
+          <h3>Angularuppgift (MovieShop)</h3>
+          <p>En fiktiv filmbutik i Angular, med hjälp av api.</p>
+          <img src={movieShop} />
           <div className="icons">
-            <a href="http://">
+            <a href="https://github.com/thereseml/MovieShop">
               <FaGithub />
             </a>
-            <a href="http://">
+            <a href="https://thereseml.github.io/MovieShop/MovieShop/products">
               <RiComputerLine />
             </a>
           </div>
         </div>
 
         <div className="flexItem">
-          <h3>Project 1</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
-            diam
-          </p>
+          <h3>Grupparbete i Angular (Katana Sushi)</h3>
+          <p>Fiktig restaurang med bokningssystem.</p>
+          <img src={katanaSushi} />
           <div className="icons">
-            <a href="http://">
+            <a href="https://github.com/GamerShabandari/react-restaurang">
               <FaGithub />
             </a>
-            <a href="http://">
-              <RiComputerLine />
-            </a>
-          </div>
-        </div>
-        <div className="flexItem">
-          <h3>Project 1</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
-            diam
-          </p>
-          <div className="icons">
-            <a href="http://">
-              <FaGithub />
-            </a>
-            <a href="http://">
-              <RiComputerLine />
-            </a>
-          </div>
-        </div>
-        <div className="flexItem">
-          <h3>Project 1</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
-            diam
-          </p>
-          <div className="icons">
-            <a href="http://">
-              <FaGithub />
-            </a>
-            <a href="http://">
-              <RiComputerLine />
-            </a>
-          </div>
-        </div>
-        <div className="flexItem">
-          <h3>Project 1</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
-            diam
-          </p>
-          <div className="icons">
-            <a href="http://">
-              <FaGithub />
-            </a>
-            <a href="http://">
+            <a href="https://gamershabandari.github.io/react-restaurang/">
               <RiComputerLine />
             </a>
           </div>
